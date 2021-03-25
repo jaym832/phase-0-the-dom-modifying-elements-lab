c9e057b3679be484442aab08537771f2e52cb284
Javier Marin <jjmarin777@gmail.com>
2021-03-25 11:56:14 -0500

    finished

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // Write your code here!
+main.remove();
+
+const newHeader=document.createElement('h1');
+document.body.appendChild(newHeader);
+newHeader.id="victory";
+
+newHeader.innerHTML="JJ is the champion";
